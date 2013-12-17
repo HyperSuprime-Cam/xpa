b4112f7b87ec03be96b2f4d03b395e53a2cede8d
Mario Juric <mjuric@lsst.org>
2013-12-16 23:44:17 -0600

    Initial import of xpa-2.1.14

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,35 @@
+# EupsPkg config file. Sourced by 'eupspkg'
+
+CONFIGURE_OPTIONS="--prefix=$PREFIX --enable-shared=yes"
+
+case $(uname) in
+	Darwin) LIBNAME="libxpa.1.0.dylib" ;;
+	Linux)	LIBNAME="libxpa.so.1.0" ;;
+esac
+
+build()
+{
+	detect_compiler
+
+	# xpa's configure fails to add -fPIC to both clang and gcc, unless the compiler executable is named 'gcc'
+	if [[ ( "$COMPILER_TYPE" == "clang" || "$COMPILER_TYPE" == "gcc" ) && "$(uname)" == "Linux" ]]; then
+		export CFLAGS="$CFLAGS -fPIC"
+	fi
+
+	make -j$NJOBS CFLAGS="$CFLAGS"
+
+	# test that the build succeeded since XPAs makefiles don't indicate
+	# failure on error
+	test -z "$LIBNAME" -o -f "$LIBNAME"
+}
+
+install()
+{
+	clean_old_install
+
+	mkdir -p "$PREFIX"
+	make -j$NJOBS install
+	test -z "$LIBNAME" -o -f "$PREFIX/lib/$LIBNAME"
+
+	install_ups
+}
